b74633b8978909eb2f611bc3fb7290441a2b8610
ssook <cesces333@naver.com>
2019-12-10 14:20:41 +0900

    댓글 입력창 색깔 추가

--- app/src/main/java/com/example/zone/SmokeHereConstants.java
+++ app/src/main/java/com/example/zone/SmokeHereConstants.java
@@ -7,6 +7,6 @@ public final class SmokeHereConstants {
     public static final int COMPANY = 3;
     public static final int STREET = 4;
     public static final int OTHER = 5;
-    public static final int BANNED = 6;
+    public static final int BANNED = 10;
     public static final int TYPECOUNT = 7;
 }
